0548655d068fd705995d62e199bbfee2d7548aaf
Nazriilhamsyah <nazril_062@smkassalaambandung.sch.id>
2023-07-21 03:04:53 -0400

    latihan vue2

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,72 @@
-<script setup>
-import { ref } from 'vue'
+<template>
+  <div>
+    <h2>Formulir</h2>
+    <form @submit.prevent="submitForm">
+      <label for="">Nama</label>
+      <input class="{'is-invalid':isInvalidName}" type="text" name="nama" v-model="kontak.nama">
+      <div v-if="isInvalidName" class="error-message">
+        Nama Harus Di Isi!
+      </div>
+      <br>
+      <label for="">Email</label>
+      <input class="{'is-invalid':isInvalidEmail}" type="email" name="email" v-model="kontak.email">
+      <div v-if="isInvalidEmail" class="error-message">
+          Email Harus Di Isi!
+        </div>
+      <br>
+      <button type="submit">Simpan</button>
+    </form>
+    <div v-if="showResult">
+      nama : {{ name }} <br>
+      email : {{ email }}
+    </div>
+  </div>
+</template>
 
-const msg = "Hello World"
-const rawHtml = "<li>Daftar 1</li>"
-const link = "https://www.youtube.com/"
-const a = 10
-const b = 5
-const c = a+b
+<script>
+import { ref } from 'vue';
+
+export default {
+  data() {
+    return {
+      kontak:
+      {
+        nama: '',
+        email: ''
+      },
+      showResult: ref(false),
+      name: ref(''),
+      email: ref(''),
+    };
+  },
+  computed: {
+    isInvalidName() {
+      return this.kontak.nama.length < 1;
+    },
+    isInvalidEmail() {
+      return this.kontak.email.length < 1;
+    }
+  },
+      methods: {
+        submitForm() {
+          this.name = this.kontak.nama;
+          this.email = this.kontak.email;
+          this.showResult = true;
+    },
+  },
+};
 </script>
 
-<template>
-  <div id="app">
-      <span>{{ msg }}</span><br>
-      <span v-html="rawHtml"></span><br>
-      <button :disabled="isButtonDisabled">Button</button><br>
-      <span v-bind="objectOfAttrs"></span><br>
-      <span style="color: red">Ini harus berwarna merah.</span><br>
-      <a v-bind:href="link"> youtube </a><br>
-      bil1={{ a }}<br>
-      bil2={{ b }}<br>
-      hasil={{ c }}
+<style>
+h1 {
+  color: green;
+}
+.error-message {
+  color: red;
+  margin-top: 5px;
+}
 
-
-    </div>
-
-</template>
-  
+.is-invalid {
+  border-color: red;
+}
+</style>
